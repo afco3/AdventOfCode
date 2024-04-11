b03b41080e5e80f201744432ec1687091c8b4a9c
Axel <ac@anova-digital.fr>
2024-04-11 23:23:38 +0200

    CCC

--- 2015/Day8/Part1/index.php
+++ 2015/Day8/Part1/index.php
@@ -11,11 +11,28 @@ foreach ($content as $string) {
     // echo $string . "\n";
     $codeLength = strlen($string);
 
-    str_replace("\\\\", "\\", $string);
-    preg_replace("/\\x[0-9a-f]{2}/", "z", $string);
-    str_replace("\\\\", "zz", $string);
+    echo $string . "\t";
+    $string = substr($string, 1, -1);
 
-    $finaleLength = strlen($string) - 2;
+    $string = str_replace('\\"', "z", $string);
+    // echo $string . "\t";
+    $string = str_replace("\\\\", "z", $string);
+    // echo $string . "\t";
+    // $string = preg_replace("/\\x[0-9a-f]{2}/", "z", $string);
+
+    while ($pos = strpos($string, "\\x")) {
+        $char1 = substr($string, $pos+2, 1);
+        $char2 = substr($string, $pos+3, 1);
+        if ((($char1 >= "0" && $char1 <= "9") || ($char1 >= "a" && $char1 <= "f")) && (($char2 >= "0" && $char2 <= "9") || ($char2 >= "a" && $char2 <= "f"))) {
+            $string = substr_replace($string, '', $pos, 3);
+        }
+    }
+    
+    echo $string . "\t";
+
+    echo "\n";
+
+    $finaleLength = strlen($string);
     $total += $codeLength - $finaleLength;
 }
 
