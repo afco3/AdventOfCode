bc69411473306b8803b76c7246f9935eab417921
COMTE Axel p2004227 <axel.comte@etu.univ-lyon1.fr>
2024-04-14 14:10:50 +0200

    day 8 part 1 + part 2 - fini

--- 2015/Day8/Part1/index.php
+++ 2015/Day8/Part1/index.php
@@ -11,7 +11,7 @@ foreach ($content as $string) {
     // echo $string . "\n";
     $codeLength = strlen($string);
 
-    echo $string . "\t";
+    // echo $string . "\t";
     $string = substr($string, 1, -1);
 
     $string = str_replace('\\"', "z", $string);
@@ -20,17 +20,18 @@ foreach ($content as $string) {
     // echo $string . "\t";
     // $string = preg_replace("/\\x[0-9a-f]{2}/", "z", $string);
 
-    while ($pos = strpos($string, "\\x")) {
+    while (($pos = strpos($string, "\\x")) !== false) {
         $char1 = substr($string, $pos+2, 1);
         $char2 = substr($string, $pos+3, 1);
+
         if ((($char1 >= "0" && $char1 <= "9") || ($char1 >= "a" && $char1 <= "f")) && (($char2 >= "0" && $char2 <= "9") || ($char2 >= "a" && $char2 <= "f"))) {
             $string = substr_replace($string, '', $pos, 3);
         }
     }
     
-    echo $string . "\t";
+    // echo $string . "\t";
 
-    echo "\n";
+    // echo "\n";
 
     $finaleLength = strlen($string);
     $total += $codeLength - $finaleLength;
