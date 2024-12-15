62c31d3aedaeb50643f2aea1f23526df652424c1
COMTE Axel p2004227 <axel.comte@etu.univ-lyon1.fr>
2024-12-15 21:52:09 +0100

    day 9 enfin !!!

--- 2024/9/part2/index.php
+++ 2024/9/part2/index.php
@@ -8,70 +8,108 @@ $partition = [];
 
 $id = 0;
 $emptyPlace = false;
+$key = 0;
 foreach (str_split($content) as $num) {
     if ($emptyPlace) {
-        $partition = array_merge($partition, [['size' => (int) $num, 'value' => "."]]);
+        $partition = array_merge($partition, [['key' => $key, 'size' => (int) $num, 'value' => "."]]);
     } else {
-        $partition = array_merge($partition, [['size' => (int) $num, 'value' => $id]]);
+        $partition = array_merge($partition, [['key' => $key, 'size' => (int) $num, 'value' => $id]]);
         $id ++;
     }
+
+    $key += $num;
 
     $emptyPlace = !$emptyPlace;
 }
 
-// displayPartition($partition);
+displayPartition($partition);
 
-
-$tmpPartition = $partition;
-$i=0;
-while (true) {
-    // if ($i>=6) dd($partition);
-    // $i++;
-    if (empty($tmpPartition)) break;
-    $fileKey = count($tmpPartition);
-    $file = array_pop($tmpPartition);
-    if (! isset($file['value']) || $file['value'] == ".") continue;
-    
-    $freeSpaceKey = array_find_key($partition, function ($space) use ($file) {
-        return $space['value'] == "." && $space['size'] >= $file['size'];
-    });
-
-    // dump($fileKey);
-    // dump($file);
-    // dump($freeSpaceKey);
-
-    if (is_null($freeSpaceKey)) continue;
-    displayPartition($partition);
-
-    array_splice($partition, $freeSpaceKey, 0, [$file]);
-    array_splice($tmpPartition, $freeSpaceKey, 0, [$file]);
-
-    $partition[$freeSpaceKey+1]['size'] = $partition[$freeSpaceKey+1]['size'] - $file['size'];
-    // dump($partition);
-    unset($partition[$fileKey]);
-
-    $partition = array_values($partition);
-
-    $firstFreeSpace = array_find_key($partition, function ($space) {
-        return $space['value'] == "." && $space['size'] > 0;
-    });
-    
-    dump($firstFreeSpace);
-    dump($fileKey);
-
-    if ($firstFreeSpace > $fileKey) break;
-}
+$reversePartition = array_reverse($partition);
 
 // dump($partition);
+// dd($reversePartition);
 
+foreach ($reversePartition as $space) {
+    dump($space['key']);
+    if ($space['value'] == '.' || $space['size'] == 0) continue;
+
+    $partition = array_reverse($reversePartition);
+
+    $newSpace = array_find($partition, function ($s) use ($space) {
+        return $s['value'] == '.' && $s['size'] >= $space['size'];
+    });
+
+    if (is_null($newSpace) || $newSpace['key'] > $space['key']) continue;
+    
+    
+    $spaceKey = array_find_key($reversePartition, function ($s) use ($space) {
+        return $s['key'] === $space['key'];
+    });
+
+    // if ($space['value'] == 2) {
+    //     dump($space);
+    //     dump($newSpace);
+    //     dump($spaceKey);
+    //     dump($reversePartition);
+    //     dump($reversePartition[$spaceKey]);
+    // }
+
+    $reversePartition[$spaceKey]['value'] = '.';
+    // if ($space['value'] == 2) {
+    //     dump($reversePartition[$spaceKey]);
+    // }
+
+    $newSpaceKey = array_find_key($reversePartition, function ($s) use ($newSpace) {
+        return $s['key'] == $newSpace['key'];
+    });
+
+    // if ($space['value'] == 2) {
+    //     dump($newSpaceKey);
+
+    //     dump($reversePartition);
+    // }
+
+    array_splice($reversePartition, $newSpaceKey+1, 0, [$space]);
+
+    // if ($space['value'] == 2) {
+    //     dump($reversePartition);
+    // }
+
+    $reversePartition[$newSpaceKey]['size'] -= $space['size'];
+
+    
+    // if ($space['value'] == 2) {
+    //     dump($reversePartition);
+    // }
+
+    $reversePartition[$newSpaceKey+1]['key'] = $reversePartition[$newSpaceKey+2]['key'] + $reversePartition[$newSpaceKey+2]['size'];
+
+    $reversePartition[$newSpaceKey]['key'] = $reversePartition[$newSpaceKey+1]['key'] + $reversePartition[$newSpaceKey+1]['size'];
+
+    
+    // if ($space['value'] == 2) {
+    //     dump($reversePartition);
+    // }
+
+    // displayPartition(array_reverse($reversePartition));
+    
+    // if ($space['value'] == 2) {
+    //     dd("");
+    // }
+}
+
+$partition = array_reverse($reversePartition);
 
 $finalpartition = [];
 
 foreach ($partition as $file) {
-    $finalpartition = array_merge($finalpartition, array_fill(0, $file['size'], $file['value']));
+    for ($i = 0; $i < $file['size']; $i++) {
+        $finalpartition[] = $file['value'];
+    }
+    // $finalpartition = array_merge($finalpartition, array_fill(0, $file['size'], $file['value']));
 }
 
-dump(implode('', $finalpartition));
+// dump(implode('', $finalpartition));
 
 $total = 0;
 foreach (array_values($finalpartition) as $key => $num) {
@@ -84,7 +122,9 @@ dd($total);
 
 // 24453448462796  too high
 
+// 13820373322856  too high
 
+// 18880851835397  too high
 
 
 
