c1d58d6ed1e6bdfb2fc8cabac0f78618353f954b
COMTE Axel p2004227 <axel.comte@etu.univ-lyon1.fr>
2024-12-19 22:29:05 +0100

    day 10

--- 2024/10/part1/index.php
+++ 2024/10/part1/index.php
@@ -11,55 +11,68 @@ $start = [];
 foreach ($content as $x => $c) {
     $content[$x] = str_split($c);
 
-    if (str_contains($c, '0')) {
+    foreach ($content[$x] as $y => $l) {
+        if ($l != 0) continue;
         $starts[] = [
             'x' => $x,
-            'y' => strpos($c, '0'),
+            'y' => $y,
         ];
     }
 }
 
+$hikes = [];
+// dd($starts);
 foreach ($starts as $start) {
-    walk($start['x'], $start['y']);
+    walk($start['x'], $start['y'], $start, $content);
 }
 
-$hikes = 0;
 
-function walk($x, $y) {
-    global $hikes, $content;
+function walk($x, $y, $start, $content) {
+    global $hikes;
     $height = $content[$x][$y];
 
-
     if ($height == 9) {
-        dump("9");
-        $hikes++;
+        $hikes[$start['x'].','.$start['y'].'_'.$x.','.$y] = $start['x'].','.$start['y'].'_'.$x.','.$y;
         return;
     }
 
     $possibility = [];
 
-    if ($content[$x-1][$y] == $height+1) {
+    if (isset($content[$x-1][$y]) && $content[$x-1][$y] == $height+1) {
         $possibility['up']['x'] = $x-1;
         $possibility['up']['y'] = $y;
     }
-    if ($content[$x][$y+1] == $height+1) {
+    if (isset($content[$x][$y+1]) && $content[$x][$y+1] == $height+1) {
         $possibility['right']['x'] = $x;
         $possibility['right']['y'] = $y+1;
     }
-    if ($content[$x+1][$y] == $height+1) {
+    if (isset($content[$x+1][$y]) && $content[$x+1][$y] == $height+1) {
         $possibility['down']['x'] = $x+1;
         $possibility['down']['y'] = $y;
     }
-    if ($content[$x][$y-1] == $height+1) {
+    if (isset($content[$x][$y-1]) && $content[$x][$y-1] == $height+1) {
         $possibility['left']['x'] = $x;
         $possibility['left']['y'] = $y-1;
     }
 
     foreach ($possibility as $pos) {
-        walk($pos['x'], $pos['y'], $content);
+        walk($pos['x'], $pos['y'], $start, $content);
     }
+
+    return;
 }
 
+dump(count($hikes));
 dd($hikes);
 
 dd($content);
+
+
+
+function displayContent($content) {
+    foreach ($content as $k => $v) {
+        $content[$k] = implode('', $v);
+    }
+
+    dump(implode("\n", $content));
+}
--- 2024/10/part2/index.php
+++ 2024/10/part2/index.php
@@ -4,4 +4,75 @@ include "../../../includes/debug.php";
 
 $content = file_get_contents("./input.txt");
 
+$content = explode("\n", $content);
+
+$start = [];
+
+foreach ($content as $x => $c) {
+    $content[$x] = str_split($c);
+
+    foreach ($content[$x] as $y => $l) {
+        if ($l != 0) continue;
+        $starts[] = [
+            'x' => $x,
+            'y' => $y,
+        ];
+    }
+}
+
+$hikes = [];
+// dd($starts);
+foreach ($starts as $start) {
+    walk($start['x'], $start['y'], $start, $content);
+}
+
+
+function walk($x, $y, $start, $content) {
+    global $hikes;
+    $height = $content[$x][$y];
+
+    if ($height == 9) {
+        $hikes[] = $start['x'].','.$start['y'].'_'.$x.','.$y;
+        return;
+    }
+
+    $possibility = [];
+
+    if (isset($content[$x-1][$y]) && $content[$x-1][$y] == $height+1) {
+        $possibility['up']['x'] = $x-1;
+        $possibility['up']['y'] = $y;
+    }
+    if (isset($content[$x][$y+1]) && $content[$x][$y+1] == $height+1) {
+        $possibility['right']['x'] = $x;
+        $possibility['right']['y'] = $y+1;
+    }
+    if (isset($content[$x+1][$y]) && $content[$x+1][$y] == $height+1) {
+        $possibility['down']['x'] = $x+1;
+        $possibility['down']['y'] = $y;
+    }
+    if (isset($content[$x][$y-1]) && $content[$x][$y-1] == $height+1) {
+        $possibility['left']['x'] = $x;
+        $possibility['left']['y'] = $y-1;
+    }
+
+    foreach ($possibility as $pos) {
+        walk($pos['x'], $pos['y'], $start, $content);
+    }
+
+    return;
+}
+
+dump(count($hikes));
+dd($hikes);
+
 dd($content);
+
+
+
+function displayContent($content) {
+    foreach ($content as $k => $v) {
+        $content[$k] = implode('', $v);
+    }
+
+    dump(implode("\n", $content));
+}
