b9ac2117fdab1984bacd59c8a45fcc40f48f7948
Axel <ac@anova-digital.fr>
2025-12-01 14:34:39 +0100

    day 1

--- 2025/1/part1/index.php
+++ 2025/1/part1/index.php
@@ -2,3 +2,29 @@
 include "../../../includes/debug.php";
 
 $content = file_get_contents("./input.txt");
+
+$instructions = explode("\n", $content);
+
+$position = 50;
+$count = 0;
+foreach ($instructions as $instruction) {
+    $direction = substr($instruction, 0, 1);
+    $number = (int) substr($instruction, 1);
+
+    if ($direction == "L") {
+        $position -= $number;
+
+        while ($position < 0) {
+            $position += 100;
+        }
+    } else {
+        $position += $number;
+        $position %= 100;
+    }
+
+    if ($position == 0) {
+        $count++;
+    }
+}
+
+dd($count);
--- 2025/1/part2/index.php
+++ 2025/1/part2/index.php
@@ -2,3 +2,42 @@
 include "../../../includes/debug.php";
 
 $content = file_get_contents("./input.txt");
+
+$instructions = explode("\n", $content);
+
+$position = 50;
+$count = 0;
+foreach ($instructions as $instruction) {
+    $direction = substr($instruction, 0, 1);
+    $number = (int) substr($instruction, 1);
+
+    if ($direction == "L") {
+        if ($position == 0) {
+            $count--;
+        }
+
+        $position -= $number;
+
+        while ($position < 0) {
+            $position += 100;
+            
+            $count++;
+        }
+        
+        if ($position == 0) {
+            $count++;
+        }
+    } else {
+        $position += $number;
+        
+        if ($position == 0) {
+            $count++;
+        }
+
+        $count += (int) ($position/100);
+
+        $position %= 100;
+    }
+}
+
+dd($count);
