12d6d738778b2da084fcde051b99525a51cb2250
Axel <ac@anova-digital.fr>
2025-12-02 16:42:51 +0100

    day 2

--- 2025/2/part1/index.php
+++ 2025/2/part1/index.php
@@ -2,3 +2,22 @@
 include "../../../includes/debug.php";
 
 $content = file_get_contents("./input.txt");
+
+$count = 0;
+foreach (explode(',', $content) as $range) {
+    [$first, $last] = explode('-', $range);
+    
+    foreach (array_fill($first, ($last-$first)+1, 0) as $id => $unused) {
+        $repeating = [];
+        preg_match('/(\d{1,'.(strlen($id)/2).'})(\1+)/', $id, $repeating);
+
+        if ($repeating[0] == $id && $repeating[1] == $repeating[2]) {
+            // dump("match !");
+            // dump($id);
+            // dump($repeating);
+            $count += $id;
+        }
+        
+    }
+}
+dd($count);
--- 2025/2/part2/index.php
+++ 2025/2/part2/index.php
@@ -2,3 +2,27 @@
 include "../../../includes/debug.php";
 
 $content = file_get_contents("./input.txt");
+
+$count = 0;
+foreach (explode(',', $content) as $range) {
+    [$first, $last] = explode('-', $range);
+
+    foreach (array_fill($first, ($last-$first)+1, 0) as $id => $unused) {
+        $repeating = [];
+
+        for ($i = 0; $i < strlen($id); $i++) {
+            preg_match('/(\d{1,'.$i.'})(\1+)/', $id, $repeating);
+
+            if ($repeating[0] == $id) {
+                // dump("match !");
+                // dump($id);
+                // dump($repeating);
+                $count += $id;
+
+                $i = 99999999999;
+            }
+        }
+    }
+}
+
+dd($count);
