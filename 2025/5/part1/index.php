0c0a97e16b94befdf93b287ad2252e62882f18c1
Axel <ac@anova-digital.fr>
2025-12-05 08:27:05 +0100

    day 5 part 1

--- 2025/5/part1/index.php
+++ 2025/5/part1/index.php
@@ -2,3 +2,44 @@
 include "../../../includes/debug.php";
 
 $content = file_get_contents("./input.txt");
+
+[$ranges, $ingredients] = explode("\n\n", $content);
+
+$freshIds = [];
+
+$ranges = explode("\n", $ranges);
+$ingredients = explode("\n", $ingredients);
+
+$count = 0;
+foreach ($ingredients as $ingredient) {
+    foreach ($ranges as $range) {
+        [$start, $end] = explode('-', $range);
+
+        if ($ingredient >= $start && $ingredient <= $end) {
+            // dump($ingredient);
+            $count++;
+            break;
+        }
+    }
+}
+
+// foreach ($ranges as $range) {
+//     [$start, $end] = explode('-', $range);
+//     $range = array_fill($start, ($end-$start)+1, '1');
+//     // dump($range);
+
+//     $freshIds = array_replace($freshIds, $range);
+// }
+
+// // dump($freshIds);
+
+// $count = 0;
+// foreach ($ingredients as $ingredient) {
+//     // dump($ingredient);
+//     if (isset($freshIds[$ingredient])) {
+//         // dump("++");
+//         $count++;
+//     }
+// }
+
+dd($count);
--- 2025/5/part2/index.php
+++ 2025/5/part2/index.php
@@ -2,3 +2,36 @@
 include "../../../includes/debug.php";
 
 $content = file_get_contents("./input.txt");
+
+$ranges = explode("\n", $content);
+
+$freshIds = [];
+
+$count = 0;
+for ($i = 0; $i < count($ranges); $i++) {
+    if (! isset($ranges[$i])) continue;
+
+    $range = $ranges[$i];
+    [$start, $end] = explode('-', $range);
+
+    for ($j = 0; $j < $i; $j++) {
+        if (! isset($ranges[$j])) continue;
+
+        $r = $ranges[$j];
+        [$s, $e] = explode('-', $r);
+
+        if ($s >= $strat && $s <= $end && $e >= $strat && $e <= $end) {
+            $ranges[] = $start.'-'.$s;
+            $ranges[] = $e.'-'.$end;
+
+        } else if ($s >= $strat && $s <= $end) {
+            dump("aa");
+        } else if ($e >= $strat && $e <= $end) {
+            dump("aaa");
+        }
+    }
+
+    $count += ($end-$start)+1;
+}
+
+dd(array_unique($freshIds));
