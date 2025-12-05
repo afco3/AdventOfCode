a4ce9705fefaa70520cc8d337b381346d30e901f
Axel <ac@anova-digital.fr>
2025-12-05 08:26:57 +0100

    day 3

--- 2025/3/part1/index.php
+++ 2025/3/part1/index.php
@@ -2,3 +2,37 @@
 include "../../../includes/debug.php";
 
 $content = file_get_contents("./input.txt");
+
+$banks = explode("\n", $content);
+
+$count = 0;
+foreach ($banks as $batteries) {
+    $batteries = str_split($batteries);
+
+    // dump(array_slice($batteries, 0, count($batteries)-1));
+
+    $max = max(array_slice($batteries, 0, count($batteries)-1));
+    
+    $joltage = $max;
+
+    $maxKey = -1;
+
+    foreach ($batteries as $key => $batterie) {
+        if ($batterie == $max) {
+            $maxKey = $key;
+            break;
+        }
+    }
+
+    $batteries = array_slice($batteries, $maxKey+1);
+
+    $max = max($batteries);
+
+    $joltage .= $max;
+
+    // dump($joltage);
+
+    $count += (int) $joltage;
+}
+
+dd($count);
--- 2025/3/part2/index.php
+++ 2025/3/part2/index.php
@@ -2,3 +2,36 @@
 include "../../../includes/debug.php";
 
 $content = file_get_contents("./input.txt");
+
+$banks = explode("\n", $content);
+
+$count = 0;
+foreach ($banks as $batteries) {
+    $batteries = str_split($batteries);
+
+    // dump(array_slice($batteries, 0, count($batteries)-1));
+
+    $joltage = "";
+    for ($i = 11; $i >= 0; $i--) {
+        $max = max(array_slice($batteries, 0, count($batteries)-$i));
+        
+        $joltage .= $max;
+
+        $maxKey = -1;
+
+        foreach ($batteries as $key => $batterie) {
+            if ($batterie == $max) {
+                $maxKey = $key;
+                break;
+            }
+        }
+
+        $batteries = array_slice($batteries, $maxKey+1);
+
+        // dump($joltage);
+
+    }
+    $count += (int) $joltage;
+}
+
+dd($count);
