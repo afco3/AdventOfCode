c590b7ef20442cfab1cea86925f732cc65d4b70b
Axel <ac@anova-digital.fr>
2025-12-06 12:47:47 +0100

    day 6

--- 2025/6/part1/index.php
+++ 2025/6/part1/index.php
@@ -2,3 +2,34 @@
 include "../../../includes/debug.php";
 
 $content = file_get_contents("./input.txt");
+
+$lines = explode("\n", $content);
+
+foreach ($lines as $key => $line) {
+    $line = preg_replace('/\s+/', ' ', trim($line));
+    $lines[$key] = explode(" ", $line);
+}
+
+$tmp = [];
+for ($x=0; $x < count($lines); $x++) {
+    for ($y=0; $y < count($lines[$x]); $y++) {
+        $tmp[$y][$x] = $lines[$x][$y];
+    }
+}
+$lines = $tmp;
+
+$count = 0;
+foreach ($lines as $calc) {
+    $symbol = array_pop($calc);
+
+    if ($symbol == '+') {
+        $result = array_sum($calc);
+    } else {
+        $result = array_product($calc);
+    }
+
+    $count += $result;
+}
+
+
+dd($count);
