3c5a5e17623cf5ffa6ffc2cd2d24ea716b951c69
Axel <ac@anova-digital.fr>
2025-12-06 15:45:27 +0100

    day 5 part 2

--- 2025/5/part2/index.php
+++ 2025/5/part2/index.php
@@ -3,35 +3,38 @@ include "../../../includes/debug.php";
 
 $content = file_get_contents("./input.txt");
 
+$content = explode("\n\n", $content)[0];
 $ranges = explode("\n", $content);
 
-$freshIds = [];
-
-$count = 0;
-for ($i = 0; $i < count($ranges); $i++) {
-    if (! isset($ranges[$i])) continue;
-
-    $range = $ranges[$i];
-    [$start, $end] = explode('-', $range);
-
-    for ($j = 0; $j < $i; $j++) {
-        if (! isset($ranges[$j])) continue;
-
-        $r = $ranges[$j];
-        [$s, $e] = explode('-', $r);
-
-        if ($s >= $strat && $s <= $end && $e >= $strat && $e <= $end) {
-            $ranges[] = $start.'-'.$s;
-            $ranges[] = $e.'-'.$end;
-
-        } else if ($s >= $strat && $s <= $end) {
-            dump("aa");
-        } else if ($e >= $strat && $e <= $end) {
-            dump("aaa");
-        }
-    }
-
-    $count += ($end-$start)+1;
+foreach ($ranges as $key => $range) {
+    $ranges[$key] = explode('-', $range);
 }
 
-dd(array_unique($freshIds));
+usort($ranges, function ($a, $b) {
+    return $a[0] > $b[0];
+});
+
+$tmp = [array_shift($ranges)];
+
+foreach ($ranges as $range) {
+    $lastTmpRangeKey = count($tmp)-1;
+    $lastTmpRange = $tmp[$lastTmpRangeKey];
+
+    if ($range[0] > $lastTmpRange[1]) {
+        $tmp[] = $range;
+        continue;
+    }
+
+    if ($range[1] > $lastTmpRange[1]) {
+        $tmp[$lastTmpRangeKey][1] = $range[1];
+    }
+}
+
+$ranges = $tmp;
+
+$count = 0;
+foreach ($ranges as $range) {
+    $count += ($range[1]-$range[0])+1;
+}
+
+dd($count);
