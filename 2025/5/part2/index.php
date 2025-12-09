dc0975ac554773730d8961f5b0faed5c62ea6e92
Axel <ac@anova-digital.fr>
2025-12-09 17:57:29 +0100

    day 5 part 2

--- 2025/5/part2/index.php
+++ 2025/5/part2/index.php
@@ -5,8 +5,6 @@ $content = file_get_contents("./input.txt");
 
 $ranges = explode("\n", $content);
 
-$freshIds = [];
-
 $count = 0;
 for ($i = 0; $i < count($ranges); $i++) {
     if (! isset($ranges[$i])) continue;
@@ -14,24 +12,54 @@ for ($i = 0; $i < count($ranges); $i++) {
     $range = $ranges[$i];
     [$start, $end] = explode('-', $range);
 
+    $d = $i == 3;
+
+    // if ($d) dump($range);
+    // if ($d) dump("<br>");
+
     for ($j = 0; $j < $i; $j++) {
         if (! isset($ranges[$j])) continue;
 
         $r = $ranges[$j];
         [$s, $e] = explode('-', $r);
+        // if ($d) dump("<br>");
 
-        if ($s >= $strat && $s <= $end && $e >= $strat && $e <= $end) {
-            $ranges[] = $start.'-'.$s;
-            $ranges[] = $e.'-'.$end;
+        // if ($d) dump($r);
+        // if ($d) dump($start);
+        // if ($d) dump($end);
+        // if ($d) dump($s);
+        // if ($d) dump($e);
+        // if ($d) dump("<br>");
 
-        } else if ($s >= $strat && $s <= $end) {
-            dump("aa");
-        } else if ($e >= $strat && $e <= $end) {
-            dump("aaa");
+        if ($s >= $start && $s <= $end && $e >= $start && $e <= $end) {
+            // if ($d) dump("1");
+            $count -= ($e-$s)+1;
+            unset($ranges[$j]);
+        } else if ($start >= $s && $start <= $e && $end >= $s && $end <= $e) {
+            // if ($d) dump("2");
+            unset($ranges[$i]);
+            continue 2;
+        } else if ($s >= $start && $s <= $end) {
+            // if ($d) dump("3");
+            $end = $s-1;
+            $ranges[$i] = $start . '-' . $end;
+            // if ($d) dump($ranges[$i]);
+        } else if ($e >= $start && $e <= $end) {
+            // if ($d) dump("4");
+            $start = $e+1;
+            $ranges[$i] = $start. '-' . $end;
+            // if ($d) dump($ranges[$i]);
         }
+        // if ($d) dump("<br>");
+        // if ($d) dump("<br>");
     }
 
     $count += ($end-$start)+1;
 }
 
-dd(array_unique($freshIds));
+// dump("<br>");
+// dump("<br>");
+// dump("<br>");
+// dump($ranges);
+
+dd($count);
