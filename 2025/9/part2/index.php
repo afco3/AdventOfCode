fbffb8e8362d7d7ec1193717ead4868869d1b347
Axel <ac@anova-digital.fr>
2025-12-11 17:53:40 +0100

    day 9 : en fait ca marche :)

--- 2025/9/part2/index.php
+++ 2025/9/part2/index.php
@@ -11,39 +11,9 @@ foreach ($points as $key => $point) {
     $points[$key] = explode(',', $point);
 }
 
-// dd($points);
-
-dump("2");
-flush();
-ob_flush();
-
 $tab = [];
 
 $delta = [];
-
-// dump("maxX : " . $maxX);
-// dump("maxY : " . $maxY);
-
-// $t = array_fill(0, $maxY, '.');
-
-// $tc = array_fill(0, $maxX, $t);
-
-// dd($tc);
-
-// for ($x=0; $x <= $maxX+1; $x++) {
-//     for ($y=0; $y <= $maxY+1; $y++) {
-//         $tab[$x][$y] = ". ";
-//     }
-    
-//     dump("x : " . $x);
-//     flush();
-//     ob_flush();
-
-// }
-
-dump("3");
-flush();
-ob_flush();
 
 foreach ($points as $key => $point) {
     if ($key == 0) $previous = $points[count($points)-1];
@@ -64,19 +34,11 @@ foreach ($points as $key => $point) {
     $tab[$point[0]][$point[1]] = "#";
 }
 
-dump("4");
-flush();
-ob_flush();
-
 $pointsByY = [];
 foreach ($tab as $x => $v) {
     foreach ($v as $y => $unused)
     $pointsByY[$y][] = $x;
 }
-
-dump("5");
-flush();
-ob_flush();
 
 $max = 0;
 
@@ -96,24 +58,6 @@ foreach ($points as $key => $point) {
         if ($x1 > $x2) continue;
         if ($y1 > $y2) continue;
 
-
-        // if (isset($tab[$x1]))
-
-        // dump($x1);
-        // dump($x2);
-        // dump($y1);
-        // dump($y2);
-
-        // $allX = array_keys(array_fill($x1, ($x2-$x1)+1, false));
-        // dump($allX); // y1 et y2
-        
-        // if (! empty($pointsByY[$y1]) && ! empty(array_intersect($allX, array_keys($pointsByY[$y1])))) continue;
-
-
-        // $allY = array_keys(array_fill($y1, ($y2-$y1)+1, false));
-        // dump($allY); // x1 et x2
-        // if (! empty($tab[$x1]) && ! empty(array_intersect($allY, array_keys($tab[$x1])))) continue;
-
         for ($x=$x1; $x <= $x2; $x++) {
             if (isset($tab[$x][$y1])) continue 2;
             if (isset($tab[$x][$y2])) continue 2;
@@ -125,12 +69,6 @@ foreach ($points as $key => $point) {
         }
 
         $max = $air;
-        // dd("");
-
-        // dump($point);
-        // dd($point2);
-
-        // $delta[implode(',', $point).";".implode(',', $point2)] = $air;
     }
 
     if (($key % 100) === 0) {
@@ -141,26 +79,4 @@ foreach ($points as $key => $point) {
 
 }
 
-dump("6");
-flush();
-ob_flush();
-
-
-
 dd($max);
-
-
-
-
-for ($i=0; $i < $maxX; $i++) {
-    for ($j=0; $j < $maxY; $j++) {
-        echo ($tab[$i][$j] ?? ".") . " ";
-    }
-    echo "<br>";
-    flush();
-    ob_flush();
-}
-
-// display($tab, true);
-
-dd($delta);
